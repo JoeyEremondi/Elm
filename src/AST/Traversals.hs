eef5643cdd3feecd3230171481320b8cf7e48b8f
jmite <jmitdase@gmail.com>
2015-07-10 12:17:07 -0600

    Generalized traversals to monadic actions

--- src/AST/Traversals.hs
+++ src/AST/Traversals.hs
@@ -6,7 +6,66 @@ import AST.Expression.Canonical as Canonical
 import AST.Expression.General
 import qualified Reporting.Annotation as A
 import qualified Data.List as List
+import Control.Applicative ((<$>), (<*>), Applicative)
+import Control.Monad (forM)
+import qualified Control.Monad.Identity as MI
 
+{-
+Given an expression a monadic action to perform with an expression,
+apply the action bottom-up to each sub-expression of this expression,
+as well as the expression itself
+-}
+mapMExpr
+  :: (Monad m, Applicative m) =>
+    (Canonical.Expr -> m Canonical.Expr) ->
+    Canonical.Expr ->
+    m Canonical.Expr
+mapMExpr fm (A.A ann e) =
+  do
+    newExpr <- newExprM
+    newAnn <- return ann --TODO remove
+    fm $ A.A newAnn newExpr
+  where
+    self = mapMExpr fm
+    newExprM =
+      case e of
+        (Literal _) -> return e
+        (Var _) -> return e
+        (Range start end) -> Range <$> self start <*> self end 
+        (ExplicitList subList) -> ExplicitList <$> forM subList self
+        (Binop op arg1 arg2) -> (Binop op) <$> self arg1 <*> self arg2
+        (Lambda pat body) -> (Lambda pat) <$> self body
+        (App fn arg) -> App <$> self fn <*> self arg
+        (MultiIf pairs) -> do
+          let (conds, exprs) = unzip pairs
+          newConds <- forM conds self
+          newExprs <- forM exprs self
+          return $ MultiIf $ zip newConds newExprs
+        (Let defs body) -> do
+          newDefs <- forM defs $ mapMDef fm
+          newBody <- self body
+          return $ Let newDefs newBody
+        (Case cexp branches) -> do
+          let (pats, branchExps) = unzip branches
+          newCexp <- self cexp
+          newExps <- forM branchExps self
+          return $ Case newCexp $ zip pats newExps
+        (Data ctor args) -> (Data ctor) <$> forM args self
+        (Access recExp field) -> Access <$> self recExp <*> return field
+        (Remove recExp field) -> Remove <$> self recExp <*> return field
+        (Insert recExp field arg) -> Insert <$> self recExp <*> return field <*> self arg
+        (Modify recExp fieldPairs) -> do
+          let (names, vals) = unzip fieldPairs
+          newVals <- forM vals self
+          newRec <- self recExp
+          return $ Modify newRec $ zip names newVals
+        (Record pairs) -> do
+          let (names, vals) = unzip pairs
+          newVals <- forM vals self
+          return $ Record $ zip names newVals
+        (Port sub) -> Port <$> mapMPort fm sub
+        (GLShader _ _ _) -> return e
+      
 
 {-
 Given an expression and an expression transforming function,
@@ -14,43 +73,36 @@ apply the function bottom-up to each sub-expression of this expression,
 as well as the expression itself
 -}
 mapExpr :: (Canonical.Expr -> Canonical.Expr ) -> Canonical.Expr -> Canonical.Expr
-mapExpr f (A.A ann e) =
-  f $ A.A ann newExpr
-  where
-    self = mapExpr f
-    newExpr =
-      case e of
-        (Literal _sub) -> e
-        (Var _sub) -> e
-        (Range sub1 sub2) -> Range (self sub1) (self sub2) 
-        (ExplicitList subs) -> ExplicitList (List.map self subs)
-        (Binop sub1 sub2 sub3) -> Binop sub1 (self sub2) (self sub3)
-        (Lambda sub1 sub2) -> Lambda sub1 (self sub2)
-        (App sub1 sub2) -> App (self sub1) (self sub2)
-        (MultiIf subs) -> MultiIf $ List.map (\(x,y) -> (self x, self y) ) subs
-        (Let defs body) -> Let (List.map (mapDef f) defs ) (self body)
-        (Case cexp branches) -> Case (self cexp) (map (\(p,ex) -> (p, self ex )) branches)
-        (Data sub1 sub2) -> Data sub1 (map self sub2)
-        (Access sub1 sub2) -> Access (self sub1) sub2
-        (Remove sub1 sub2) -> Remove (self sub1) sub2
-        (Insert sub1 sub2 sub3) -> Insert (self sub1) sub2 (self sub3)
-        (Modify sub1 sub2) -> Modify (self sub1) sub2
-        (Record subs) -> Record $ map (\(s,ex) -> (s, self ex) ) subs
-        (Port sub) -> Port $ mapPort f sub
-        (GLShader sub1 sub2 sub3) -> GLShader sub1 sub2 sub3
+mapExpr f e =
+  let
+    fm x = return $ f x
+  in MI.runIdentity $ mapMExpr fm e
 
 --Apply an expression transformer to the right-hand side of a definition
-mapDef :: (Canonical.Expr -> Canonical.Expr ) -> Canonical.Def -> Canonical.Def
-mapDef f (Definition lhs rhs maybeTipe) =
-  Definition lhs (mapExpr f rhs) maybeTipe
+mapMDef
+  :: (Monad m, Applicative m)
+  => (Canonical.Expr -> m Canonical.Expr )
+  -> Canonical.Def
+  -> m Canonical.Def
+mapMDef f (Definition lhs rhs maybeTipe) =
+  do
+    newRHS <- mapMExpr f rhs
+    return $ Definition lhs newRHS maybeTipe
 
 --Apply an expression transformer to each expression in a port declaration
-mapPort :: (Canonical.Expr -> Canonical.Expr ) -> PortImpl Canonical.Expr t -> PortImpl Canonical.Expr t 
-mapPort f portImpl =
+mapMPort
+  :: (Monad m, Applicative m)
+  => (Canonical.Expr -> m Canonical.Expr )
+  -> PortImpl Canonical.Expr t
+  -> m (PortImpl Canonical.Expr t) 
+mapMPort f portImpl =
   case portImpl of 
     (In p1 p2) ->
-      In p1 p2
-    (Out p1 p2 p3) ->
-      Out p1 (mapExpr f p2) p3
-    (Task p1 p2 p3) ->
-      Task p1 (mapExpr f p2) p3
+      return $ In p1 p2
+    (Out p1 p2 p3) -> do
+      newExpr <- mapMExpr f p2
+      return $ Out p1 newExpr p3
+    (Task p1 p2 p3) -> do
+      newExpr <- mapMExpr f p2
+      return $ Task p1 newExpr p3
+
